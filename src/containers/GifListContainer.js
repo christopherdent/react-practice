e6b47bc0c10e8a827fd8becd5a9a9f2fdd4ce011
Christopher Dent <christopherdent01@gmail.com>
2021-05-30 15:37:20 -0400

    first commit

---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+// the App component should render out the GifListContainer component
+
+const GifListContainer = () => {
+  return (
+    <div>
+        <h1>I'm the container</h1>
+
+    </div>
+  )
+}
+
+export default GifListContainer
